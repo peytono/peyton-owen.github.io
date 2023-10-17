96a14b3d6fbac0235bec33c211f158294628bf16
Peyton Owen <pcopcoeco@gmail.com>
2023-10-17 03:43:10 +0000

    some additions to variables.js

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,26 +1,30 @@
 /**
  *  VARIABLES: 
  * 
- * 0: Variables are nemed containers for data in JavaScript. We create variables using declarative key words...
+ * 0: To hold things in memory during the life-cycle of a program we can use variables. Variables are named identifies that can point to values of a particular type, like a Number, String, Boolean, Array, Object or another data-type. Variables are called so because once created, we can CHANGE the value (and type of a value) to which they point.
  * 
- * 1. Declaration and assignment: To create a variable in JavaScript we must first DECLARE it using one of three declaritive keywords.
+ * 1. Declaration and assignment: To create a variable in JavaScript we must first DECLARE it using one of three declaritive keywords. Assignment is the first time a variable is given or 'assigned' a value.
  * 
- * 2: Var, let, const: var, let and const are the three declarative keywords.
+ * 2: Var, let, const: var, let and const are the three declarative keywords. The three keywords have different uses. They differ in rules when it comes to initialization, redeclaring, reassigning, and scope. 
  * 
- * 3: Hoisting: (describe hoisting)
+ * 3: Hoisting: The process by which your program can move the declaration of functions, variables, classes, or imports to the beginning or their scope, before executing code.
  * 
  */
 
 // 1. Declaration and Assignment //
 
-// Declaration
-// At the declaration phase...
+// Declaration 
+// At the declaration phase either var, let, or const should be used to declare the variable. Following the key word a space should be used, followed by a name that has correlation
 var age; 
 
 // Assignment
 // age is now assigned the number 35 as its value
 age = 35;
 
+
 // 2. Var, let and const //
 
 // var //
+
+
+// 3. Hoisting //
