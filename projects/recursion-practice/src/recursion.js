c0a8ed866cfd30506a3772faaa62810a547c3611
Peyton Owen <pcopcoeco@gmail.com>
2023-11-03 18:51:36 +0000

    recursion.js: palindrome()

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -155,7 +155,23 @@ var reverse = function(string, output = '') {
 };
 
 // 10. Write a function that determines if a string is a palindrome.
-var palindrome = function(string) {
+var palindrome = function(string, stringAltered = '', reversed = '') {
+  // base
+  if (reversed.length === string.length){
+    if(reversed.toLowerCase().split(' ').join('') === string.toLowerCase().split(' ').join('')){
+      return true;
+    } else { return false; }
+  }
+  // recursion
+  
+  if(reversed.length === 0){
+    let stringAltered = string;
+    reversed = stringAltered[0];
+    return palindrome(string, stringAltered.slice(1), reversed);
+  } else {
+    reversed = stringAltered[0] + reversed;
+    return palindrome(string, stringAltered.slice(1), reversed);
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
